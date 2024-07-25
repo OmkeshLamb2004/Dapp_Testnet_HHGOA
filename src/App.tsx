e82ca0bf0098963c1725f291841a483535dc6681
Omkesh Lamb <omkesh2004lamb@gmail.com>
2024-07-25 13:41:39 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TransactionsComponent from "./TransactionComponent";
 import Balances from "./Balances";
 import { useGardenSetup } from "./store";
 import "./App.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   useGardenSetup();
